91a29c80e21a18a1af182c612bf727dee1ea936f
Lucas Carvalho <lucasccorrea@gmail.com>
2018-10-11 14:56:54 -0300

    Add comanders, init test file of cli

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,7 +1,14 @@
-import { expect } from 'chai';
+const expect = require('chai').expect;
+const exec = require('child_process').exec;
 
-describe('Main', () => {
-  it('shoul be true', () => {
-    expect(true).to.be.true;
+const btcConverter = './src/main.js';
+
+describe('Main CLI', () => {
+  it('shoul return Hello Word', (done) => {
+    exec(btcConverter, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal('Hello Word');
+      done();
+    });
   });
 });
