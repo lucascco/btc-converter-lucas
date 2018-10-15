bd2c6ef94957b644ae650c98b3c95c4c03c7c7b2
Lucas Carvalho <lucasccorrea@gmail.com>
2018-10-15 14:13:35 -0300

    add ora spinner

--- bin/BTConverter.js
+++ bin/BTConverter.js
@@ -4,12 +4,21 @@ var request = require('request');
 
 var chalk = require('chalk');
 
+var ora = require('ora');
+
+var spinner = ora({
+  text: 'Retriving Values',
+  color: 'yellow'
+});
+
 function BTConverter() {
   var current = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'BRL';
   var amount = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
   var url = "https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=".concat(current, "&amount=").concat(amount);
+  spinner.start();
   request(url, function (error, response, body) {
     try {
+      spinner.stop();
       var apiResponse = JSON.parse(body);
       console.log("".concat(chalk.red(amount), " BTC to ").concat(chalk.blue(current), " = ").concat(chalk.green(apiResponse.price)));
     } catch (erroApi) {
--- src/BTConverter.js
+++ src/BTConverter.js
@@ -1,11 +1,18 @@
 const request = require('request');
 const chalk = require('chalk');
+const ora = require('ora');
+
+const spinner = ora({
+  text: 'Retriving Values',
+  color: 'yellow',
+});
 
 function BTConverter(current = 'BRL', amount = 1) {
   const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${current}&amount=${amount}`;
-
+  spinner.start();
   request(url, (error, response, body) => {
     try {
+      spinner.stop();
       const apiResponse = JSON.parse(body);
       console.log(`${chalk.red(amount)} BTC to ${chalk.blue(current)} = ${chalk.green(apiResponse.price)}`);
     } catch (erroApi) {
